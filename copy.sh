b19586dcf204df6e1bebfe2e84d540b3946be95c
sangeetha-partha <84180410+sangeetha-partha@users.noreply.github.com>
2021-06-29 16:37:31 +0530

    Add files via upload

---
+++ copy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+echo Hice@8437chi | sudo -S rm -rf /home/1972001/AutomationDesktopUI
+
+echo Hice@8437chi | sudo -S git clone http://sangeetha-partha:Alliswell%40123%23@github.com/sangeetha-partha/AutomationDesktopUI.git
+echo Hice@8437chi | pwd
+cd /home/1972001/AutomationDesktopUI
+echo Hice@8437chi | sudo ls
+echo Hice@8437chi | sudo tar -xvf BDDDesktopUIAutomation.tar.xz 
+
+echo Hice@8437chi | sudo -S cp -R /home/1972001/AutomationDesktopUI/BDDDesktopUIAutomation /var/lib/docker/volumes/DesktopUIAutomation_vol/_data
+
+echo Hice@8437chi | sudo -S rm -rf /home/1972001/AutomationDesktopUI
+
+
+
